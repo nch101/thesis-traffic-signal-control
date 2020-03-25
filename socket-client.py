86041fe26e2ae90df308ae327247eeab3a5cf821
= <=>
2020-03-25 22:05:21 +0700

    path socket io

--- socket-client.py
+++ socket-client.py
@@ -1,8 +1,23 @@
-import sockeio
+import socketio
+import time
 
-sio = sockeio.Client()
+sio = socketio.Client()
+sion = socketio.ClientNamespace()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.on('news', namespace = '/data-collect')
+def my_event(data):
+    print('Received data: ', data)
+    for i in range (0, 30):
+        sio.emit('news', i, '/data-collect')
+        time.sleep(3)
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
 
 sio.connect('http://localhost:3000')
-@sio.on('connect')
-def connect_handler():
-    print('Connected!')
+sio.wait()
