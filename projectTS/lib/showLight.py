5e7f03eba904ae4bf77e1ed8fc042cc15dad09f0
= <=>
2020-05-20 21:15:03 +0700

    create showLight class

---
+++ projectTS/lib/showLight.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+
+class showLight():
+    def __init__(self, red_pin, yellow_pin, green_pin):
+        self.red_pin = red_pin
+        self.yellow_pin = yellow_pin
+        self.green_pin = green_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.red_pin, GPIO.OUT)
+        GPIO.setup(self.yellow_pin, GPIO.OUT)
+        GPIO.setup(self.green_pin, GPIO.OUT)
+
+    def showLight(self, status):
+        self.__turnoffAllLights()
+        if (status == 'red'):
+            GPIO.output(self.red_pin, GPIO.HIGH)
+        else if (status == 'yellow'):
+            GPIO.output(self.yellow_pin, GPIO.HIGH)
+        else if (status == 'green'):
+            GPIO.output(self.green_pin, GPIO.HIGH)
+    
+    def __turnoffAllLights():
+        GPIO.output(self.red_pin, GPIO.LOW)
+        GPIO.output(self.yellow_pin, GPIO.LOW)
+        GPIO.output(self.green_pin, GPIO.LOW)
